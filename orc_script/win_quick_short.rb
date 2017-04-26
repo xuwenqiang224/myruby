b54f7abbe57e861db3009d0a75029e0480bcd7ea
kixu <kids.xu@amd.com>
2017-04-26 15:33:08 +0800

    new file not finishing

---
+++ orc_script/win_quick_short.rb
@@ -0,0 +1,17 @@
+#quick to setting parameters run vmin
+#
+#Author: Wenqiang.Xu
+#
+#rev draft
+##########
+
+require 'orclib'
+include Orclib::MsgModule
+
+class Run_Vmin
+
+  attr_accessor
+  def initialize
+    @folder
+  end
+end
